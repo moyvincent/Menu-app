97ded780cf24e138a74e4f5bc5c5d87851a02162
user <moyakivincent@yahoo.com>
2022-08-07 04:39:00 +0100

    Modified the terraform template

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,11 +13,45 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+#EC2 instance
 resource "aws_instance" "menu_server" {
   ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
+  key_name = "eksKeyPair"
 
   tags = {
     Name = "Menuproject"
+    project = "Menu_app"
   }
 }
+
+#Security group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic on port 22 and 5000"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 5000
+    to_port          = 5000
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  tags = {
+    Name = "allow_tls"
+    project = "Menu_app"
+  }
+}
+
