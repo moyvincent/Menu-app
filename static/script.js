43882538cf7995033d77b3bbf591a19661a12792
Vincent Moyaki <moyakivincent@yahoo.com>
2023-07-25 19:37:25 +0100

    Filestack API update

--- static/script.js
+++ static/script.js
@@ -2,15 +2,18 @@ const client = filestack.init('ATf068C1VQ3KS17hx3YiAz');
 
 const options = {
     fromSources: ["local_file_system"],
-    accept: ['.png, .jpeg'],
+    accept: ['.png', '.jpeg'],
     acceptFn: (file, options) => {
-    const mimeFromExtension = options.mimeFromExtension(file.originalFile.name);
-    if(options.acceptMime.length && !options.acceptMime.includes(mimeFromExtension)) {
-        return Promise.reject('Cannot accept that file')
-    }
-    return Promise.resolve()
+        const mimeFromExtension = options.mimeFromExtension(file.originalFile.name);
+        if (options.acceptMime.length && !options.acceptMime.includes(mimeFromExtension)) {
+            return Promise.reject('Cannot accept that file')
+        }
+        return Promise.resolve()
     },
-    onUploadDone: function(data){
+    storeTo: {
+        workflows: ["5e80b68c-fe69-4778-a7fc-99d0e48e5786"]
+    },
+    onUploadDone: function (data) {
         console.log(data);
 
         for (file of data.filesUploaded) {
@@ -24,10 +27,9 @@ const options = {
             li.innerText = file.filename;
             document.getElementById("uploaded-filenames").appendChild(li);
         }
-    },
-    fromSources: ["local_file_system"],
+    }
 }
-document.getElementById("upload-btn").addEventListener("click", function(event){
+document.getElementById("upload-btn").addEventListener("click", function (event) {
     event.preventDefault();
     client.picker(options).open();
 })
